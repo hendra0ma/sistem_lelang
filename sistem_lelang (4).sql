030c1991987df5fe6d3a3e3513addd4dfcb31ddd
hendra0ma <hendra0maulidan@gmail.com>
2021-03-21 23:51:58 +0700

    revisi

--- sistem_lelang (4).sql
+++ sistem_lelang (4).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 14, 2021 at 05:29 AM
+-- Generation Time: Mar 21, 2021 at 05:51 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.4.13
 
@@ -34,6 +34,13 @@ CREATE TABLE `history_lelang` (
   `id_user` int(11) NOT NULL,
   `penawaran_harga` int(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `history_lelang`
+--
+
+INSERT INTO `history_lelang` (`id_history`, `id_lelang`, `id_barang`, `id_user`, `penawaran_harga`) VALUES
+(6, 12, 12, 7, 1620);
 
 -- --------------------------------------------------------
 
@@ -79,8 +86,8 @@ CREATE TABLE `tb_barang` (
 --
 
 INSERT INTO `tb_barang` (`id_barang`, `nama_barang`, `tgl`, `harga_awal`, `deskripsi_barang`, `gambar_barang`, `id_user`) VALUES
-(8, 'Angklung', '2021-03-14', 1200, 'angklunggg', '604d8ca7214241615695015angklung.jpg', 4),
-(9, 'gendang', '2021-03-14', 123123123, 'barang mudah dipakai', '604d8dadc2bf716156952771605112933-bola.png', 4);
+(12, 'gendang', '2021-03-21', 1200, 'barang jadul', '605771e1470551616343521Kendang.jpg', 7),
+(13, 'Gambus', '2021-03-21', 5000, 'gak tau', '60577553a479d1616344403saluang.jpg', 7);
 
 -- --------------------------------------------------------
 
@@ -96,16 +103,18 @@ CREATE TABLE `tb_lelang` (
   `id_user` int(11) DEFAULT NULL,
   `id_petugas` int(11) DEFAULT NULL,
   `status` enum('dibuka','ditutup','menunggu') NOT NULL,
-  `user_bid` int(11) DEFAULT NULL
+  `user_bid` int(11) DEFAULT NULL,
+  `kelipatan` int(11) DEFAULT NULL,
+  `fee_petugas` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tb_lelang`
 --
 
-INSERT INTO `tb_lelang` (`id_lelang`, `id_barang`, `tgl_lelang`, `harga_akhir`, `id_user`, `id_petugas`, `status`, `user_bid`) VALUES
-(8, 9, '2021-03-18', '1200', 4, 8, 'dibuka', NULL),
-(9, 8, '2021-03-14', '123123123', 4, 8, 'dibuka', NULL);
+INSERT INTO `tb_lelang` (`id_lelang`, `id_barang`, `tgl_lelang`, `harga_akhir`, `id_user`, `id_petugas`, `status`, `user_bid`, `kelipatan`, `fee_petugas`) VALUES
+(12, 12, '2021-03-21', '1620', 7, 8, 'ditutup', 4, 300, 180),
+(13, 13, '2021-03-21', '5000', 7, 8, 'dibuka', NULL, 500, NULL);
 
 -- --------------------------------------------------------
 
@@ -150,7 +159,9 @@ CREATE TABLE `tb_masyarakat` (
 
 INSERT INTO `tb_masyarakat` (`id_user`, `nama_lengkap`, `email`, `username`, `password`, `telp`, `id_level`, `gambar`) VALUES
 (4, 'hendra maulidan', 'hendra0maulidan@gmail.co', 'hendra', '$2y$10$DGFDMQyd8.zpH30a2odNgOBiR6G4YX8XLL.dpkM/bkT0Fs0Y7.4Sm', '089087767863', 1, 'U-1615649121FB_IMG_1593712356053.jpg'),
-(5, 'hendra maulidan', 'hendra@mail.com', 'budiHartanto', '$2y$10$I6tmZR29QjQzWb1ZXPxC2.MEmL87uXOrIBNM33OYzo1dRZMk5hSRm', '09809890', 1, '');
+(5, 'hendra maulidan', 'hendra@mail.com', 'budiHartanto', '$2y$10$I6tmZR29QjQzWb1ZXPxC2.MEmL87uXOrIBNM33OYzo1dRZMk5hSRm', '09809890', 1, ''),
+(6, 'wibu', 'wibu@baubawang.com', 'wibu', '$2y$10$z92bJejRrU0J7a5S/6MyUe/.Bfd.jy/5LpNAZySCiOEPq1ZWknbLG', '08392893829', 1, ''),
+(7, 'budi', 'budi@mail.com', 'budi', '$2y$10$/ZvZyFbrEEx4AMCfv7P3HuTtI2byhYCTKAANutmKk8JjLOWJA.owu', '08989233', 1, '');
 
 -- --------------------------------------------------------
 
@@ -229,7 +240,7 @@ ALTER TABLE `tb_petugas`
 -- AUTO_INCREMENT for table `history_lelang`
 --
 ALTER TABLE `history_lelang`
-  MODIFY `id_history` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_history` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `tb_administrator`
@@ -241,13 +252,13 @@ ALTER TABLE `tb_administrator`
 -- AUTO_INCREMENT for table `tb_barang`
 --
 ALTER TABLE `tb_barang`
-  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `tb_lelang`
 --
 ALTER TABLE `tb_lelang`
-  MODIFY `id_lelang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_lelang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `tb_level`
@@ -259,7 +270,7 @@ ALTER TABLE `tb_level`
 -- AUTO_INCREMENT for table `tb_masyarakat`
 --
 ALTER TABLE `tb_masyarakat`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `tb_petugas`
